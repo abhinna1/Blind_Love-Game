ff87d51785fc82fabf8eacb299f670c04107d5be
manan <manandharabhinna@gmail.com>
2021-04-20 09:59:26 +0545

    Heart Added

--- gamee.py
+++ gamee.py
@@ -8,30 +8,33 @@ cloud1_y = -100
 cloud2_x = 800
 cloud2_y = -50
 
+char_sprite=0
 char_x=0 #x position of character
 char_y=467 #y position of character
-char_velocity=5
+char_velocity=10
+walkcount=0
+
+
 class game:
     def __init__(self,win_x,win_y):
         self.win_x=win_x
         self.win_y=win_y
         self.screen=pygame.display.set_mode((self.win_x,self.win_y))
-
+        pygame.display.set_caption('Blind Love')
+        self.moving = False
 
     def sky(self):
         self.sky_bg=pygame.image.load('png files/Sky.png').convert_alpha()
         self.sky_bg=pygame.transform.scale((self.sky_bg),(1000,1000))
         self.screen.blit(self.sky_bg,(0,0))
-
     def ground(self):
         self.ground_img=pygame.image.load('png files/Ground.png').convert_alpha()
         self.screen.blit(self.ground_img,(0,70))
-
-    def heart(self):
+    def heart(self,heart_x,heart_y):
+        self.heart_rect=pygame.Rect(heart_x,heart_y,30,30)
         self.heart_img=pygame.image.load('png files/Heart Animation/Heart1.png')
-        self.heart_img=pygame.transform.scale2x(self.heart_img)
-        self.screen.blit(self.heart_img,(200,460))
-
+        #self.heart_img=pygame.transform.scale2x(self.heart_img)
+        self.screen.blit(self.heart_img,(heart_x,heart_y))
 
     def cloud_movement(self):
         global cloud1_x
@@ -53,49 +56,66 @@ class game:
         self.screen.blit(self.cloud2_img,(cloud2_x,cloud2_y))
         game_instance.cloud_movement()
 
+    def character(self):#load in character frames
+        self.char_rect=pygame.Rect(char_x,char_y,30,30)
+        self.key=pygame.key.get_pressed()
+        self.char_list=[pygame.image.load('png files/Still Animation/Still Character Animation1.png').convert_alpha(),pygame.image.load('png files/Still Animation/Still Character Animation2.png').convert_alpha()]
+    def character_anim(self):#character animations
+        global walkcount
 
-    def character(self):
-
-        self.char_list=[]
-        self.char_list.append(pygame.image.load('png files/Still Animation/Still Character Animation1.png').convert_alpha())
-        self.char_list.append(pygame.image.load('png files/Still Animation/Still Character Animation2.png').convert_alpha())
-        self.char_sprite=0
-        self.screen.blit(self.char_list[self.char_sprite],(char_x,char_y))
+        if self.moving==False: #boolean self.moving is defined in th __init__ function
+            self.char_out=self.screen.blit(self.char_list[walkcount],(char_x,char_y))
+            walkcount+=1
+        pygame.display.update()
     def char_movement(self):
         global char_x
         global char_y
+        global walkcount
         self.key=pygame.key.get_pressed()
-        if self.key[pygame.K_d]:
+        if self.moving==False:
+            if walkcount>=2:
+                walkcount=0
+        #right movement
+        if (self.key[pygame.K_d]or self.key[pygame.K_RIGHT]) and char_x<930:
+            self.moving=False
             char_x+=char_velocity
-        if self.key[pygame.K_a]:
+
+        #Left movement
+        if (self.key[pygame.K_a]or self.key[pygame.K_LEFT])and char_x>-25:
             char_x-=char_velocity
-        if self.key[pygame.K_w]:
+        #Jump
+        if self.key[pygame.K_w]or self.key[pygame.K_UP]:
             pass
-        if self.key[pygame.K_s]:
-            pass
+    def heart_collide(self):
+        if self.char_rect==self.heart_rect:
+            print('hi')
 
-    def update(self):
-        self.char_sprite=self.char_sprite+1
-        if self.char_sprite>len(self.char_list):
-            self.char_sprite = 0
-
-
-
+#gameloop
 run=True
 while run:
     game_instance=game(1000, 600)
-    game_instance.sky()
-    game_instance.ground()
+
+    game_instance.sky()#sky background
+    game_instance.ground()#ground
+    game_instance.clouds()#clouds
+
+    #All character Functions are here
     game_instance.character()
     game_instance.char_movement()
-    game_instance.heart()
-    game_instance.clouds()
+    game_instance.character_anim()
 
-    game_instance.update()
+    
+    game_instance.heart(200,450)
+    game_instance.heart_collide()
+
+
+
+
 
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type==pygame.QUIT or game_instance.key[pygame.K_ESCAPE]:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-    clock.tick(60)
+    pygame.display.flip()
+    clock.tick(120)
--- main.py
+++ main.py
@@ -1,44 +1,21 @@
 import pygame,sys
-pygame.init()#initializing pygame
+import csv,os
+pygame.init()
+screen=pygame.display.set_mode((500,500))
+run=True
 
-screen=pygame.display.set_mode((1000,600))#Window Resolution
-title=pygame.display.set_caption('Blind Love')#Window Title
+list=[[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
-clock=pygame.time.Clock()
-
-ground=pygame.transform.scale(pygame.image.load('png files/Ground.png'),(1000,500))
-sky=pygame.image.load('png files/Sky.png')
-sky=pygame.transform.scale(sky,(1000,1000))
-clouds=pygame.transform.scale(pygame.image.load('png files/clouds.png'),(700,600))
-
-#sky=pygame.transform.scale2x(sky)
-character = pygame.image.load('png files/Still Animation/Still Character Animation1.png')
-
-#Character info
-char_x=50
-char_y=432
-char_vel=5
-
-while True:
-    key = pygame.key.get_pressed()
-
-    screen.blit(sky, (0, -50))
-    screen.blit(clouds, (-100, -50))
-    screen.blit(clouds, (500, -100))
-    screen.blit(ground, (0, 120))
-    screen.blit(character, (char_x, char_y))
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            quit()
-            sys.exit()
-        if key[pygame.K_d]or key[pygame.K_RIGHT]:
-            char_x=char_x+char_vel
-        if key[pygame.K_a]or key[pygame.K_LEFT]:
-            char_x=char_x-char_vel
-
-
-
-
-
-    pygame.display.update()
-    clock.tick(120)
+for event in pygame.event.get():
+    if event.type==pygame.QUIT:
+        pygame.quit()
+        sys.exit()
